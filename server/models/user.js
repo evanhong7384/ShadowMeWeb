cdcc346d25811cc2ae119ce622fe665c38fea9f7
Evan Hong <evanhong@dhcp-10-29-9-105.dyn.mit.edu>
2023-01-19 12:58:57 -0500

    changed user scheme to encorporate institution etc.

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: String,
   googleid: String,
+  institution: String,
+  resume: String,
+  linkedin: String,
+  location: String,
+  bio: String,
 });
 
 // compile model from schema
